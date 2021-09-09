59f4ab909408f41c6edc876407d86c0241db7736
tragiclov <frijacblu@yahoo.com>
2021-09-09 14:10:12 -0700

    Index almost done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+    const body = document.querySelector('body');
+
+    const header = document.querySelector('.main-navigation');
+    header.addEventListener('mouseover', function(){
+        header.style.backgroundColor = makeRanColor();
+    });
+    const makeRanColor = () => {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        return 'rgb(${r}, ${g}, ${b})';
+    }
+     
+    
---
+++ stretch-assignment/index.js
@@ -0,0 +1,18 @@
+// Your code goes here
+    const buttons = document.querySelectorAll('.btn');
+    const aTags = document.querySelectorAll('nav-link');
+    const pictureHeightlight = document.querySelectorAll(".img-content");
+    const bod = document.querySelector('html');
+    const head = document.querySelector('.main-navigation')
+
+    buttons.forEach(button =>{
+        button.addEventListener('click', event => {
+            button.getElementsByClassName.backgroundColor = 'red'
+            console.log(event)
+        })
+        button.addEventListener('mouseleave', () =>{
+            button.style.backgroundColor = 'white'
+            button.style.color = 'green'
+        })
+    })
+    
